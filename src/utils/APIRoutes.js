402c1c470546893dc4167f69fd609b9a7d107e79
JonathanMRuiz <jonimrz@yahoo.com>
2023-02-08 15:51:18 -0300

    frontend host

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,5 @@
-export const host = "http://localhost:5000";
+export const host =
+  "https://realtime-chat-backend-production-a330.up.railway.app/";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setavatar`;
